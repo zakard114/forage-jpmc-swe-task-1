3cd7bbda0bfa6cf8fefeb0b0277310dc8d2275e3
zakard114 <infantry_hooa@hotmail.com>
2024-08-08 13:50:23 +1000

    Fix getDataPoint and getRatio functions

--- client3.py
+++ client3.py
@@ -26,8 +26,8 @@ import urllib.request
 QUERY = "http://localhost:8080/query?id={}"
 
 # 500 server request
-N = 500
-
+N = 10
+# 500
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
@@ -35,14 +35,17 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    if(price_b == 0):
+        # when price_b is 0 avoid throwing ZeroDivisionError
+        return
+    return price_a / price_b
 
 
 # Main
@@ -52,8 +55,11 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        if "ABC" in prices and "DEF" in prices:
+            print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
